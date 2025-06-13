a00243e190af621e5993cba0b1f695a39f08e885
aaina1472 <aainasrivastav@gmail.com>
2025-06-13 17:02:52 +0530

    app_py.py

--- app_py.py
+++ app_py.py
@@ -369,8 +369,8 @@ elif st.session_state.page == "📝 Feedback":
         st.markdown("### 🙏 We appreciate your time!")
         st.info("Your feedback helps us improve. Stay happy and healthy!")
 
-# Continue button to go to Feedback page
-if st.button("➡️ Continue to Feedback"):
+    # Continue button to go to Feedback page
+    if st.button("➡️ Continue to Feedback"):
     st.session_state.page = "📝 Feedback"
     st.rerun()
 
